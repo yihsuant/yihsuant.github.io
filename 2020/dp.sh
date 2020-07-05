3e58700fd7c70a0a68dbedcdad7d843dba75b0d8
blair <blair.values@gmail.com>
2020-07-05 09:27:10 +0800

    rm all

--- 2020/dp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "start deploy my blogyih..."
-
-git pull && git status && git add * && git status && git commit -m "add or modify article" && git push origin master
-
-echo "successful commit to blogyih source."
